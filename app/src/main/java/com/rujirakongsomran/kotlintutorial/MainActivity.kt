92ac61d63e8c6d5e84c2a100108797937f7055d1
RujiraKongsomran <rujirabiwberry@gmail.com>
2020-06-18 11:37:49 +0700

    Functions

--- app/src/main/java/com/rujirakongsomran/kotlintutorial/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/kotlintutorial/MainActivity.kt
@@ -2,11 +2,73 @@ package com.rujirakongsomran.kotlintutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    fun sum2number(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    fun minus2number(a: Int, b: Int): Int {
+        return a - b
+    }
+
+    fun multi2number(a: Int, b: Int): Int {
+        return a * b
+    }
+
+    fun divided2number(a: Float, b: Float): Float {
+        if (b != 0f)
+            return a / b
+        else
+            return 0f;
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnSum.setOnClickListener {
+            Toast.makeText(
+                this,
+                "" + sum2number(etNum1.text.toString().toInt(), etNum2.text.toString().toInt()),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        btnMinus.setOnClickListener {
+            Toast.makeText(
+                this,
+                "" + minus2number(
+                    etNum1.text.toString().toInt(),
+                    etNum2.text.toString().toInt()
+                ),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        btnMulti.setOnClickListener {
+            Toast.makeText(
+                this,
+                "" + multi2number(
+                    etNum1.text.toString().toInt(),
+                    etNum2.text.toString().toInt()
+                ),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        btnDivided.setOnClickListener {
+            Toast.makeText(
+                this,
+                "" + divided2number(
+                    etNum1.text.toString().toFloat(),
+                    etNum2.text.toString().toFloat()
+                ),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
-    
 }
